3e7338dbe9153032e01fe10d083a83a1908c0a7c
Ady Moussa <adymoussa.edu@gmail.com>
2018-01-18 09:12:06 +0100

    Update db_config.js

--- config/db_config.js
+++ config/db_config.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
 	host     : 'localhost',
 	user     : 'root',
 	password : '',
-	database : 'login_programmerblognet'
+	database : 'login_0118_omskrevet_eksempel'
 });
 
 connection.connect();
